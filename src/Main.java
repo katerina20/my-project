77879050da338ec86f8a06d556297580f0d4cf91
Kateryna Oblakevych <malutina.catya14@gmail.com>
2022-12-29 09:11:35 +0200

    Homework #2. Added long variable.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 12;
         int b = 16;
+        long v = 12;
         System.out.println("Hello world! " + a + b);
     }
 }
